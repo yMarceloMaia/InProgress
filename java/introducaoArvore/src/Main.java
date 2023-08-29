4e4af7ff0cec68fc867bc45b7daa58c93a531666
yMarceloMaia <marcelomaiajp@gmail.com>
2023-08-28 22:59:27 -0300

    pensar e responder introdução a arvores concluido

--- java/introducaoArvore/src/Main.java
+++ java/introducaoArvore/src/Main.java
@@ -1,7 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
 
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Digite um número, caso queira parar o programa digite o número zero"));
+
+        while(num != 0){
+            num = Integer.parseInt(JOptionPane.showInputDialog("Digite um número, caso queira parar o programa digite o número zero"));
+            tree.insertNode(num);
+        }
+
+        tree.showInOrder();
     }
 }
